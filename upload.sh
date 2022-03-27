2ef638d918119988dbc57371ea196921ccaf43f3
siddharth1729 <siddharth.sabron@gmail.com>
2022-03-27 14:43:52 +0530

    uploading sh file

---
+++ upload.sh
@@ -0,0 +1,13 @@
+
+    #!/bin/bash
+    # Ask the user for their name
+echo Hello, Pleas enter your commit
+read varname
+echo It\'s nice to meet you $varname
+
+
+git add .
+git commit -m "$varname"
+git push
+
+git status
