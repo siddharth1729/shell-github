e12c7480f236ce07383ecc6e57671be9a1071688
siddharth1729 <siddharth.sabron@gmail.com>
2022-03-27 15:03:58 +0530

    status

--- upload.sh
+++ upload.sh
@@ -6,13 +6,13 @@ read varname
 echo It\'s nice to meet you $varname
 
 echo "\n____________________________________________________\n"
-echo "\n_________________ADDING AND PUSHING FILES____________\n"
+echo "_________________ADDING AND PUSHING FILES____________\n"
 
 git add .
 git commit -m "$varname"
 git push
 
 echo "\n____________________________________________________\n"
-echo "\n_________________GIT STATUS____________\n"
+echo "_________________GIT STATUS____________\n"
 git status
 echo "\n____________________________________________________\n"
